415528141be2098bf99794284d119c56931ce9b4
justhere213 <kirby.balding@aol.com>
2024-03-24 18:29:35 -0600

    Final version

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function openSettings() {
+    document.getElementById("overlay").style.display = "flex";
+}
+  
+function closeSettings() {
+    document.getElementById("overlay").style.display = "none";
+}
